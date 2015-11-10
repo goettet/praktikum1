ea33efe7e730c97c564f4126c6430e7fa5673e82
goettet <goettet@philnet-pc0037.philnet.ad.uni-koeln.de>
2015-11-10 11:11:19 +0100

    Banane java

---
+++ objects/src/objects/Banane.java
@@ -0,0 +1,21 @@
+package objects;
+
+public class Banane {
+	
+	String color;
+	Integer weight;
+	
+	void setColor(String cl) {
+		color = cl;
+	}
+	
+	String getColor() {
+		return color;
+	}
+	
+	Integer getWeight() {
+		return weight;
+		
+	}
+
+}
