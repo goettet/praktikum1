c09cca2df55d717d8cffc070f631c41994a2bc2f
goettet <goettet@philnet-pc0037.philnet.ad.uni-koeln.de>
2015-11-10 10:26:09 +0100

    helloworld project

---
+++ praktikum1/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		HelloWorld hello = new HelloWorld(); //-> auf klasse verweisen
+		hello.printHelloWorld();
+	}
+               void printHelloWorld() {
+	           System.out.println("Hello World");
+   }
+}
